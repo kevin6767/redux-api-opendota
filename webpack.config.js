c368e484c98a1f9935dcd196c549bb918b339b25
Kevin Eslick <eslick.kevin@yahoo.com>
2020-05-19 16:08:10 -0500

    new build

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-    entry:  ['./dist/bundle.js'],
+    entry:  ["./dist/bundle.js"],
     mode: "development",
     module: {
         rules: [
